de9e8a2280e950fc37ba05e8def7e2dfc60f1194
Abhiram1819 <abhiramravipati9@gmail.com>
2021-12-18 19:11:16 +0530

    updated home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:todo_app/screens/add_task.dart';
 
 class Home extends StatefulWidget {
@@ -9,6 +10,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var tasks = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,8 +28,63 @@ class _HomeState extends State<Home> {
         ),
         backgroundColor: Colors.blue,
         onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => addtask()));
+          // Navigator.push(
+          //     context, MaterialPageRoute(builder: (context) => addtask()));
+          showModalBottomSheet<void>(
+            // context and builder are
+            // required properties in this widget
+            context: context,
+            isScrollControlled: true,
+            builder: (BuildContext context) {
+              // we set up a container inside which
+              // we create center column and display text
+              return FractionallySizedBox(
+                heightFactor: 0.9,
+                child: Container(
+                  padding:EdgeInsets.only(top:10,right: 30,left:30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        child: Text('Add New Task',style: TextStyle(
+  	                    fontWeight: FontWeight.bold, fontSize: 28, color: Colors.blue),),
+                      ),SizedBox(height: 30,),
+                      Container(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter Task'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter Description'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Add Task',
+                            style: GoogleFonts.roboto(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
         },
       ),
     );
